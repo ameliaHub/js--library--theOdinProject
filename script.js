e45929dbaeb495e3bae17c8fc47e8b783cb4037d
pilarvermu <pilaribermu@gmail.com>
2025-01-30 11:45:39 +0100

    Añadida funcionalidad con dialog

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function Book(title, numberOfPages, author,read){
 function addBookToLibrary(title, numberOfPages, author,read){
     book = new Book(title, numberOfPages, author,read);
     library.push(book);
+    displayBooks();
 }
 
 // Ejemplo de uso
@@ -36,9 +37,40 @@ function displayBooks(){
         libraryContainer.appendChild(bookElement);
 
     })
-
-
 }
+
+//Manejo de los botones del formulario
+const newBookButton = document.getElementById("newBookButton");
+const bookForm = document.getElementById("bookForm");
+const bookDialog = document.getElementById("bookDialog");
+const cancelButton = document.getElementById("cancelButton");
+
+//Mostrar el formulario cuando se haga click en "Nuevo libro"
+newBookButton.addEventListener("click", ()=>{
+    bookDialog.showModal();
+});
+
+//Ocultar el formulario cuando se haga click en "Cancelar"
+cancelButton.addEventListener("click", ()=>{
+    bookDialog.close();
+});
+
+//Manejo de los valores del formulario
+bookForm.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    addBookToLibrary(title, pages, author, read);
+
+    bookForm.reset();
+    bookDialog.close();
+});
+
+
 
 displayBooks();
 
