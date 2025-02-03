a6eac6149fb19c733f8b013ba6c10d49a9977018
pilarvermu <pilaribermu@gmail.com>
2025-02-03 21:39:03 +0100

    Libro y librería reescritas con clases

--- script.js
+++ script.js
@@ -1,89 +1,93 @@
 const library = [];
 
-//cosntructora
-function Book(title, numberOfPages, author,read){
-    this.title=title;
-    this.numberOfPages=numberOfPages;
-    this.author=author;
-    this.read=read;
+
+class Book{
+    constructor(title, numberOfPages, author,read){
+        this.title=title;
+        this.numberOfPages=numberOfPages;
+        this.author=author;
+        this.read=read;
+    }
+
+    toggleReadStatus(){
+        this.read = !this.read;
+    }
 }
 
+class Library{
+    constructor(){
+        this.books = [];
+    }
 
-//alternar el estado de lectura (lo comparten todos los objetos libro y al usarlo en vez
-//de crear una funcion en cada objeto usamos prototype y no duplicamos codigo)
-Book.prototype.toggleReadStatus = function(){
-    this.read = !this.read;
+    addBook(title, numberOfPages, author, read){
+        const book = new Book(title, numberOfPages, author, read);
+        this.books.push(book);
+        this.displayBooks();
+    }
+
+    removeBook(index){
+        this.books.splice(index,1);
+        this.displayBooks();
+    }
+
+    toggleReadStatus(index){
+        this.books[index].toggleReadStatus();
+        this.displayBooks();
+    }
+
+    displayBooks(){
+        const libraryContainer = document.getElementById("libraryContainer");
+        libraryContainer.innerHTML = "";
+
+        this.books.forEach((book, index) =>{
+            const bookElement = document.createElement("div");
+            bookElement.classList.add("book");
+
+            bookElement.innerHTML = `
+                <h3>${book.title}</h3>
+                <p>Autor: ${book.author}</p>
+                <p>Páginas: ${book.numberOfPages}</p>
+                <p>Leído: ${book.read ? "Sí" : "No"}</p>
+                <button class="removeBookButton" data-index="${index}">Eliminar</button>
+                <label>
+                    <input type="checkbox" class="readStatusCheckbox" data-index="${index}" ${book.read ? "checked" : ""}>
+                    Leido
+                </label>
+            `;
+
+            libraryContainer.appendChild(bookElement);
+
+            //añadimos el evento de eliminación al botón de cada libro
+            const removeBookButton = bookElement.querySelector(".removeBookButton");
+            removeBookButton.addEventListener("click", (event)=>{
+                const indexToRemove = event.target.getAttribute("data-index");
+                this.removeBook(indexToRemove);
+            })
+
+            //checkbox de leído
+            const readStatusCheckbox = bookElement.querySelector(".readStatusCheckbox");
+            readStatusCheckbox.addEventListener("change", (event)=>{
+                const indexToToggle = event.target.getAttribute("data-index");
+                this.toggleReadStatus(indexToToggle);
+            })
+
+
+        })
+    }
+
+    removeBook(index){
+        this.books.splice(index,1)
+        this.displayBooks()
+    }
 }
 
-function addBookToLibrary(title, numberOfPages, author,read){
-    book = new Book(title, numberOfPages, author,read);
-    library.push(book);
-    displayBooks();
-}
 
 // Ejemplo de uso
-addBookToLibrary("1984", 328, "George Orwell", false);
-addBookToLibrary("El Principito", 96, "Antoine de Saint-Exupéry",true);
 
-function displayBooks(){
-    const libraryContainer = document.getElementById("libraryContainer");
+const myLibrary = new Library();
 
-    libraryContainer.innerHTML = "";
-
-    library.forEach((book, index) =>{
-        const bookElement = document.createElement("div");
-        bookElement.classList.add("book");
-
-        bookElement.innerHTML = `
-            <h3>${book.title}</h3>
-            <p>Autor: ${book.author}</p>
-            <p>Páginas: ${book.numberOfPages}</p>
-            <p>Leído: ${book.read ? "Sí" : "No"}</p>
-            <button class="removeBookButton" data-index="${index}">Eliminar</button>
-            <label>
-                <input type="checkbox" class="readStatusCheckbox" data-index="${index}" ${book.read ? "checked" : ""}>
-                Leido
-            </label>
-        `;
-        
-        //<button class="toggleReadButton" data-index="${index}">Leído</button>
-
-        libraryContainer.appendChild(bookElement);
-
-        //añadimos el evento de eliminación al botón de cada libro
-        const removeBookButton = bookElement.querySelector(".removeBookButton");
-        removeBookButton.addEventListener("click", (event)=>{
-            const indexToRemove = event.target.getAttribute("data-index");
-            removeBookFromLibrary(indexToRemove);
-        })
-
-        //añadimos el evento de cambio de estado de lectura (USANDO BOTÓN EN VEZ DE CHECKBOX)
-        //const toggleReadButton = bookElement.querySelector(".toggleReadButton");
-        //toggleReadButton.addEventListener("click", (event)=>{
-          //  const indexToToggle = event.target.getAttribute("data-index");
-          //  toggleReadStatus(indexToToggle);
-        //})
-
-        const readStatusCheckbox = bookElement.querySelector(".readStatusCheckbox");
-        readStatusCheckbox.addEventListener("change", (event)=>{
-            const indexToToggle = event.target.getAttribute("data-index");
-            toggleReadStatus(indexToToggle);
-        })
-
-    })
-}
-
-
-function removeBookFromLibrary(index){
-    library.splice(index,1);
-    displayBooks();
-}
-
-function toggleReadStatus(index){
-    const book =library[index];
-    book.toggleReadStatus();
-    displayBooks();
-}
+myLibrary.addBook("1984", 328, "George Orwell", false);
+myLibrary.addBook("El Principito", 96, "Antoine de Saint-Exupéry",true);
 
 //Manejo de los botones del formulario
 const newBookButton = document.getElementById("newBookButton");
@@ -110,7 +114,7 @@ bookForm.addEventListener("submit", (event)=>{
     const pages = document.getElementById("pages").value;
     const read = document.getElementById("read").checked;
 
-    addBookToLibrary(title, pages, author, read);
+    myLibrary.addBook(title, pages, author, read);
 
     bookForm.reset();
     bookDialog.close();
@@ -118,6 +122,5 @@ bookForm.addEventListener("submit", (event)=>{
 
 
 
-displayBooks();
 
-console.log(library);
+
